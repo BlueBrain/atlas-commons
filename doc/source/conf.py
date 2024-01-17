81f4eb31f8f894c88962e4d6d6a663baac7a9cc3
MikeG <michael.gevaert@epfl.ch>
2024-01-17 09:54:35 +0100

    fix building docs (#8)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'atlas-building-tools'
+project = 'atlas-commons'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -108,7 +108,7 @@ html_theme = 'sphinx-bluebrain-theme'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = u'atlas-building-tools'
+html_title = u'atlas-commons'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -184,7 +184,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'atlas-building-tools.tex', u'atlas-building-tools Documentation',
+  ('index', 'atlas-commons.tex', u'atlas-commons Documentation',
    u'BlueBrain NSE', 'manual'),
 ]
 
